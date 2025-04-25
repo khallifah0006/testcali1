44e24e29894e9a9e8d074e4add2206739fd11161
khallifah0006 <khallifah.825220006@stu.untar.ac.id>
2025-04-25 19:43:17 +0700

    first commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve index.html file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname,  'index.html'));
 });
 
 // Array untuk menyimpan data BMI (dalam aplikasi produksi, gunakan database)
